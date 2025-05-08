196fa9efafb4bc4a309e5bc0a1c7879b653e3a98
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-05-08 20:39:23 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name = var.resource_group_name
     storage_account_name = var.storage_account_name
     container_name = var.container_name
-    key = "opentofu.tfstate"
+    key = var.resource_group_name
    } 
 } 
