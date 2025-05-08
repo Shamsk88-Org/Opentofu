cb07d8997793ed2515e08c2dbef8fef44be3d900
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-05-08 20:26:13 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -21,7 +21,3 @@ description = "ResourceLocation"
 variable "container_name" {
 description = "ResourceLocation"
 }
-
-variable "resource_group_name" {
-description = "The name of the resource group name"
-}
