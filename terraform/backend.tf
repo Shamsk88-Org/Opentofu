ceeb6d0e65b8785f08dd1754ae35576799e6c054
shamsherkhan <shamsherkhan.@cognizant.com>
2025-09-11 15:16:21 +0530

    updated provider

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
-## Backend file #
-terraform {
-  backend "azurerm" {
-    resource_group_name = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name = var.container_name
-    key = var.resource_name
-    use_azuread_auth = true
-   } 
-} 
+# ## Backend file #
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name = var.resource_group_name
+#     storage_account_name = var.storage_account_name
+#     container_name = var.container_name
+#     key = var.resource_name
+#     use_azuread_auth = true
+#    } 
+# } 
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,14 +2,21 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.6.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.resource_name
+    use_azuread_auth     = true
   }
 }
 
 provider "azurerm" {
   features {}
-  tenant_id = var.tenant_id
+  tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
